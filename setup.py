d166a3d4c2a38dfbfdf1019484593526d308e593
Andrew Williams <56925856+AndrewWilliams3142@users.noreply.github.com>
2021-01-25 10:31:45 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='icontools',
+      version='0.1',
+      description='Utility functions for analysing ICON output',
+      url='http://github.com/AndrewWilliams3142/icontools',
+      author='Andrew Williams',
+      author_email='andrew.williams@physics.ox.ac.uk',
+      license='GNU',
+      packages=['icontools'],
+      install_requires=[],
+      zip_safe=False)
